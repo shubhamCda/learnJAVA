2e0e8dd51af96658b2047dfd3d21da4bdbf5ef4d
shubhamCda <shubhambodalkar281@gmail.com>
2026-09-03 04:14:22 +0530

    Add correct id functionality in users

--- src/main/java/com/demo/first/app/User.java
+++ src/main/java/com/demo/first/app/User.java
@@ -6,7 +6,7 @@ public class User {
     private String email;
 
     public User(int id, String name, String email) {
-        id = id;
+        this.id = id;
         this.name = name;
         this.email = email;
     }
