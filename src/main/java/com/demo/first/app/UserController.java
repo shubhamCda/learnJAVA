32b9de72b549e83efd4c924ff5b7dfd61ff635d4
shubhamCda <shubhambodalkar281@gmail.com>
2026-09-02 17:54:32 +0530

    Add @RestController annotation

--- src/main/java/com/demo/first/app/UserController.java
+++ src/main/java/com/demo/first/app/UserController.java
@@ -3,10 +3,12 @@ package com.demo.first.app;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
